29d5136b36541de6285f6534fdb68a386987eecb
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2024-01-15 08:25:56 +0100

    Updates

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function sumarArray(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            throw new Error(`El valor ${array[i]} no es un número. En realidad su tipo es ${typeof(array[i])}.`);
+        }
+    }
+
     let suma = 0;
     
     for (let i = 0; i < array.length; i++) {
@@ -9,6 +19,16 @@ function sumarArray(array) {
 }
 
 function filtrarPares(pares) {
+    if (pares.length === 0) {
+        return undefined;
+    }
+    
+    for (let i = 0; i < pares.length; i++) {
+        if (isNaN(pares[i])) {
+            throw new Error(`El valor ${pares[i]} no es un número. En realidad su tipo es ${typeof(pares[i])}.`);
+        }
+    }
+    
     return pares.filter(function (numero) {
         return numero % 2 === 0;
     });
@@ -20,6 +40,12 @@ function encontrarMaximo(array) {
         return undefined;
     }
     
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            throw new Error(`El valor ${array[i]} no es un número. En realidad su tipo es ${typeof(array[i])}.`);
+        }
+    }
+
     let maximo = array[0];
     
     for (let i = 1; i < array.length; i++) {
@@ -32,6 +58,11 @@ function encontrarMaximo(array) {
 }
 
 function contarOcurrencias(array, valor) {
+    if (array.length === 0) {
+        return undefined;
+    } else if (isNaN(valor)) {
+        throw new Error(`El valor ${valor} no es un número. En realidad su tipo es ${typeof(valor)}.`);
+    }
     let count = 0;
     
     for (let i = 0; i < array.length; i++) {
@@ -45,12 +76,26 @@ function contarOcurrencias(array, valor) {
 
 
 function concatenarArrays(array1, array2) {
+    if (array1.length === 0 && array2.length === 0) {
+        return undefined;
+    } else if (array1.length === 0) {
+        return array2;
+    } else if (array2.length === 0) {
+        return array1;
+    }
+
     return array1.concat(array2);
 }
 
 function encontrarMinimo(array) {
     if (array.length === 0) {
         return undefined;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            throw new Error(`El valor ${array[i]} no es un número. En realidad su tipo es ${typeof(array[i])}.`);
+        }
     }
     
     let minimo = array[0];
@@ -66,6 +111,16 @@ function encontrarMinimo(array) {
 
 
 function calcularPromedio(array) {
+    if (array.length === 0) {   
+        return undefined;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            throw new Error(`El valor ${array[i]} no es un número. En realidad su tipo es ${typeof(array[i])}.`);
+        }
+    }
+
     let suma = sumarArray(array);
     let promedio = suma / array.length;
     return promedio;
@@ -86,14 +141,20 @@ let arrayCopia = copiarArray(arrayOriginal);
 console.log(arrayCopia);
 
 function verificarElemento(array, elemento) {
+    if (array.length === 0) {
+        return undefined;
+    } else if (isNaN(elemento)) {
+        throw new Error(`El valor ${elemento} no es un número. En realidad su tipo es ${typeof(elemento)}.`);
+    }
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] === elemento) {
-            console.log("El elemento existe en el array.");
+            console.log(`El elemento ${elemento} existe en el array.`);
             return;
         }
     }
     
-    console.log("El elemento no existe en el array.");
+    console.log(`El elemento ${elemento} no existe en el array.`);
 }
 
 let arrayOriginal2 = [1, 2, 3, 4, 5];
@@ -101,6 +162,12 @@ let elemento = 3;
 verificarElemento(arrayOriginal2, elemento);
 
 function contarMayores(array, valor) {
+    if (array.length === 0) {
+        return undefined;
+    } else if (isNaN(valor)) {
+        throw new Error(`El valor ${valor} no es un número. En realidad su tipo es ${typeof(valor)}.`);
+    }
+
     let contador = 0;
     
     for (let i = 0; i < array.length; i++) {
@@ -121,6 +188,10 @@ newArray.sort();
 console.log(newArray);
 
 function duplicarArray(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+
     let newArray = [];
 
     for (let i = 0; i < array.length; i++) {
@@ -132,6 +203,10 @@ function duplicarArray(array) {
 }
 
 function duplicarArray2(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+
     let newArray = [];
 
     for (let i = 0; i < array.length; i++) {
@@ -157,6 +232,12 @@ for (let i = 0; i < claves.length; i++) {
 console.log(objetoCombinado);
 
 function encontrarPosicion(array, elemento) {
+    if (array.length === 0) {
+        return undefined;
+    } else if (isNaN(elemento)) {
+        throw new Error(`El valor ${elemento} no es un número. En realidad su tipo es ${typeof(elemento)}.`);
+    }
+    
     for (let i = 0; i < array.length; i++) {
         if (array[i] === elemento) {
             return i;
